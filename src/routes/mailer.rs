e0290335ed32f011a7979b569c045af448d8b146
Ovenoboyo <ovenoboyo@gmail.com>
2023-06-06 20:24:57 +0530

    Update deployment stuff
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/routes/mailer.rs
+++ src/routes/mailer.rs
@@ -1,9 +1,19 @@
+use std::env;
+
 use axum::Router;
 
 use crate::{handlers::mailer::mail_handler, utils::google_token::TokenHandler};
 
 pub fn routes() -> Router {
-    let state = TokenHandler::new("D:\\Projects\\Work\\Greenie\\utils\\keys\\local\\googleapi\\service-account-key.json".to_string(), "https://www.googleapis.com/auth/gmail.send".to_string()).unwrap();
+    let mut key = env::current_dir().unwrap();
+    key.push("./keys/local/googleapi/service-account-key.json");
+
+    let state = TokenHandler::new(
+        key.to_string_lossy().to_string(),
+        "https://www.googleapis.com/auth/gmail.send".to_string(),
+    )
+    .unwrap();
     axum::Router::new()
-        .route("/mailer/send_mail", axum::routing::post(mail_handler)).with_state(state)
+        .route("/mailer/send_mail", axum::routing::post(mail_handler))
+        .with_state(state)
 }
