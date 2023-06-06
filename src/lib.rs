db44c452bf41a797f08b3b6ace61b5b747e57bc3
Ovenoboyo <ovenoboyo@gmail.com>
2023-06-06 20:09:39 +0530

    Add deployment config
    Signed-off-by: Ovenoboyo <ovenoboyo@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,15 @@
-use axum::{
-    routing::get,
-    Router,
-};
-use std::net::SocketAddr;
+use axum::{routing::get, Router};
 use dotenv::dotenv;
+use std::net::SocketAddr;
 
 mod errors;
 pub use errors::{Error, Result};
-pub mod routes;
-pub mod handlers;
-pub mod dtos;
-pub mod services;
 pub mod db;
+pub mod dtos;
+pub mod handlers;
 pub mod models;
+pub mod routes;
+pub mod services;
 pub mod utils;
 
 pub async fn build_run() {
@@ -20,12 +17,13 @@ pub async fn build_run() {
 
     // let db_client = db::Database::get_client().await.unwrap();
 
-    let app= Router::new().merge(routes::mailer::routes())
+    let app = Router::new()
+        .merge(routes::mailer::routes())
         .route("/health-check", get(|| async { "All Ok!" }));
 
     // let app = app.with_state(db_client);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3030));
     println!("Server started, listening on {addr}");
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
